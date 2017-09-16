fba5c9a8c025725d7a7b143d56c2ce921d9954aa
Abhishek <tyagi766@gmail.com>
2017-09-17 02:26:39 +0530

    Tried seperating urls for apps but failed (find a way to include url configs from seperate apps)

--- lists/urls.py
+++ lists/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from lists import views as list_views
 
+app_name = 'lists'
 urlpatterns = [
-	url(r'^$', list_views.home_page, name='home'),
-
-	# TODO seperate these urls in their specific apps
-
-	# lists urls
 	url(r'^lists/(\d+)/$', list_views.view_list, name = 'view_list'),
 	url(r'^lists/(\d+)/add_item$', list_views.add_item, name = 'add_item'),
 	url(r'^lists/new$', list_views.new_list, name = 'new_list'),
