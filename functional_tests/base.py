f8382aac3515acc1ca3f9be9873e24b986135028
Abhishek Tyagi <abhishek.tyagi766@appstreet.io>
2017-09-29 14:44:25 +0530

    enforce model validation in list view

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,33 +7,34 @@ from unittest import skip
 import time
 import os
 
+
 class FunctionalTest(StaticLiveServerTestCase):
-	@classmethod
-	def setUpClass(cls):
-		if 'liveserver' in os.environ:
-			cls.server_url = 'http://' + os.environ['liveserver']
-			return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
+    @classmethod
+    def setUpClass(cls):
+        if 'liveserver' in os.environ:
+            cls.server_url = 'http://' + os.environ['liveserver']
+            cls.live_server_url = ''
+            return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
 
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-		
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
 
-		self.assertIn(row_text, [row.text for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
 
 
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,42 +1,43 @@
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTest
 
 import time
 
+
 class ItemValidationTest(FunctionalTest):
-	def test_cannot_add_empty_list_items(self):
-		# Edith goes to the home page and accidentally tries to submit 
-		# an empty list item. She hits Enter on the empty input box
-		self.browser.get(self.server_url)
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
-		# The home page refreshes, and there is an error message saying
-		# that list items cannot be blank
-		error = self.browser.find_element_by_css_selector('.has-error')
-		self.assertEqual(error.text, "You can't have an empty list item")
+        time.sleep(2)
 
-		# She tries again with some text for the item, which now works
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-		self.check_for_row_in_list_table('1: Buy milk')
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
-		# Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        # She tries again with some text for the item, which now works
+        input_field = self.browser.find_element_by_id('id_new_item')
+        input_field.send_keys('Buy milk')
+        input_field.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy milk')
 
-		# She receives a similar warning on the list page
-		self.check_for_row_in_list_table('1. Buy milk')
-		error = self.browser.find_element_by_css_selector('.has-erro')
-		self.assertEqual(error.text, "You can't have an empty list item")
+        # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
-		# And she can correct it by filling some text in
-		self.check_for_row_in_list_table('1. Buy milk')
-		self.browser.find_element_by_id('id_new_item', "Make tea")
-		self.check_for_row_in_list_table('2. Make tea')
-		self.fail('write me!')
+        # She receives a similar warning on the list page
+        time.sleep(2)
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
-
-
-
-
-
-
-
-
+        # And she can correct it by filling some text in
+        self.check_for_row_in_list_table('1: Buy milk')
+        input_field = self.browser.find_element_by_id('id_new_item')
+        input_field.send_keys("Make tea")
+        input_field.send_keys(Keys.ENTER)
+        time.sleep(2)
+        self.check_for_row_in_list_table('2: Make tea')
