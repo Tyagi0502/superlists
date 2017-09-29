bc126b947473a9bc73a6074d913f199daacf149c
Abhishek Tyagi <abhishek.tyagi766@appstreet.io>
2017-09-29 15:13:32 +0530

    Refactor hard coded urls out of server

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from lists import views as list_views
 
 urlpatterns = [
-	url(r'^$', list_views.home_page, name='home'),
-	url(r'^lists/', include('lists.urls')),
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include('lists.urls', namespace='lists')),
 ]
